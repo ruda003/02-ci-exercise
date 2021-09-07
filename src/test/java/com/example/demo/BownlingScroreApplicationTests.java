f29e1153c6a0c9c1e126662273add18c8037f5c4
Ruben <ruben.dahl@protonmail.ch>
2021-09-07 11:11:31 +0200

    added new test

--- src/test/java/com/example/demo/BownlingScroreApplicationTests.java
+++ src/test/java/com/example/demo/BownlingScroreApplicationTests.java
@@ -9,5 +9,12 @@ class BownlingScroreApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	void shouldHandleTheMostSimpleCasePossible() {
+		Game g = new Game();
+		g.roll(4);
+		assertEquals(g.score(), 4);
+		g.roll(2);
+		assertEquals(g.score(), 6);
+	}
 }
