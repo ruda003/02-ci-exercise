b22e7370ac70ce10dd1c905b46871e8a06854a2a
Ruben <ruben.dahl@protonmail.ch>
2021-09-07 11:17:32 +0200

    fatter ingenting

--- src/test/java/com/example/demo/BownlingScroreApplicationTests.java
+++ src/test/java/com/example/demo/BownlingScroreApplicationTests.java
@@ -9,12 +9,4 @@ class BownlingScroreApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	@Test
-	void shouldHandleTheMostSimpleCasePossible() {
-		Game g = new Game();
-		g.roll(4);
-		assertEquals(g.score(), 4);
-		g.roll(2);
-		assertEquals(g.score(), 6);
-	}
 }
