a46d630854bcd323879ec34149bca05cbce0c48a
glennbech <glenn.bech@gmail.com>
2021-09-07 07:43:52 +0200

    Oh forgot this one (this is a very good comment.. frequent one as well)

---
+++ src/main/java/com/example/demo/BowlingScoreApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BowlingScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BowlingScoreApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+
+
+
+        };
+    }
+
+
+}
